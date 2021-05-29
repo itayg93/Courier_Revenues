908df3dd48d05b32820f4ff7aa0a8226a5403069
Itay <itayg93@gmail.com>
2021-05-30 00:35:03 +0300

    Fix input bugs in ProfileScreen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,5 +1,13 @@
 import React, { useContext, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Keyboard,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { Avatar, TextInput, Button, Switch } from "react-native-paper";
 
 import { Formik } from "formik";
@@ -25,141 +33,156 @@ export const ProfileScreen = () => {
   const [isSubmitExpenses, setIsSubmitExpenses] = useState(false);
   const onToggleSwitch = () => setIsSubmitExpenses(!isSubmitExpenses);
   return (
-    <AppScreen style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
-      {/** profile */}
-      <View style={styles.profileDetailsContainer}>
-        <Avatar.Icon icon="account" backgroundColor={AppColors.medium} />
-        <View style={styles.displayNameAndEmailWrapper}>
-          <Text style={styles.displayName}>{displayName}</Text>
-          <Text style={styles.email}>{email}</Text>
-        </View>
-      </View>
-      {/** is submit expenses */}
-      <View style={styles.isSubmitExpensesContainer}>
-        <Text style={styles.isSubmitExpenesText}>Submit Expenses?</Text>
-        <Switch
-          value={isSubmitExpenses}
-          onValueChange={onToggleSwitch}
-          color={AppColors.primary}
-        />
-      </View>
-      {/** tax, commission and insurances */}
-      <Formik
-        initialValues={{
-          taxPoints: "",
-          commissionRate: "",
-          compulsoryInsurance: "",
-          collateralInsurance: "",
-          personalInsurance: "",
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(values) => {
-          setIsLoading(true);
-          // call firestore func
-          console.log(isSubmitExpenses);
-          console.log(values);
-        }}
-      >
-        {({
-          handleChange,
-          handleSubmit,
-          values,
-          setFieldTouched,
-          touched,
-          errors,
-        }) => (
-          <>
-            {/** tax points and commission rate */}
-            <View style={styles.taxPointsAndCommissionRateContainer}>
-              {/** tax */}
-              <TextInput
-                style={styles.taxPointsInput}
-                label="Tax Points"
-                onChangeText={handleChange("taxPoints")}
-                onBlur={() => setFieldTouched("taxPoints")}
-                value={values.taxPoints}
-                keyboardType="numeric"
-                autoCorrect={false}
-              />
-              {errors["taxPoints"] && touched["taxPoints"] ? (
-                <Text style={styles.errorMessage}>{errors["taxPoints"]}</Text>
-              ) : null}
-              {/** commission */}
-              <TextInput
-                style={styles.commissionRateInput}
-                label="Commission Rate"
-                onChangeText={handleChange("commissionRate")}
-                onBlur={() => setFieldTouched("commissionRate")}
-                value={values.commissionRate}
-                keyboardType="numeric"
-                autoCorrect={false}
-              />
-              {errors["commissionRate"] && touched["commissionRate"] ? (
-                <Text style={styles.errorMessage}>
-                  {errors["commissionRate"]}
-                </Text>
-              ) : null}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <AppScreen style={styles.container}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={{ flex: 1, justifyContent: "flex-end" }}>
+            <Text style={styles.text}>Profile Screen</Text>
+            {/** profile */}
+            <View style={styles.profileDetailsContainer}>
+              <Avatar.Icon icon="account" backgroundColor={AppColors.medium} />
+              <View style={styles.displayNameAndEmailWrapper}>
+                <Text style={styles.displayName}>{displayName}</Text>
+                <Text style={styles.email}>{email}</Text>
+              </View>
             </View>
-            {/** insurances */}
-            <Text style={styles.insurancesTitle}>Insurances</Text>
-            {/** compulsory */}
-            <TextInput
-              label="Compulsory Insurance"
-              onChangeText={handleChange("compulsoryInsurance")}
-              onBlur={() => setFieldTouched("compulsoryInsurance")}
-              value={values.compulsoryInsurance}
-              keyboardType="numeric"
-              autoCorrect={false}
-            />
-            {errors["compulsoryInsurance"] && touched["compulsoryInsurance"] ? (
-              <Text style={styles.errorMessage}>
-                {errors["compulsoryInsurance"]}
-              </Text>
-            ) : null}
-            {/** collateral */}
-            <TextInput
-              style={styles.textInput}
-              label="Collateral Insurance"
-              onChangeText={handleChange("collateralInsurance")}
-              onBlur={() => setFieldTouched("collateralInsurance")}
-              value={values.collateralInsurance}
-              keyboardType="numeric"
-              autoCorrect={false}
-            />
-            {errors["collateralInsurance"] && touched["collateralInsurance"] ? (
-              <Text style={styles.errorMessage}>
-                {errors["collateralInsurance"]}
-              </Text>
-            ) : null}
-            {/** personal */}
-            <TextInput
-              style={styles.textInput}
-              label="Personal Insurance"
-              onChangeText={handleChange("personalInsurance")}
-              onBlur={() => setFieldTouched("personalInsurance")}
-              value={values.personalInsurance}
-              keyboardType="numeric"
-              autoCorrect={false}
-            />
-            {errors["personalInsurance"] && touched["personalInsurance"] ? (
-              <Text style={styles.errorMessage}>
-                {errors["personalInsurance"]}
-              </Text>
-            ) : null}
-            {/** update btn */}
-            <Button
-              style={styles.updateBtn}
-              mode="contained"
-              onPress={handleSubmit}
-              loading={isLoading}
+
+            {/** is submit expenses */}
+            <View style={styles.isSubmitExpensesContainer}>
+              <Text style={styles.isSubmitExpenesText}>Submit Expenses?</Text>
+              <Switch
+                value={isSubmitExpenses}
+                onValueChange={onToggleSwitch}
+                color={AppColors.primary}
+              />
+            </View>
+            {/** tax, commission and insurances */}
+            <Formik
+              initialValues={{
+                taxPoints: "",
+                commissionRate: "",
+                compulsoryInsurance: "",
+                collateralInsurance: "",
+                personalInsurance: "",
+              }}
+              validationSchema={validationSchema}
+              onSubmit={(values) => {
+                setIsLoading(true);
+                // call firestore func
+                console.log(isSubmitExpenses);
+                console.log(values);
+              }}
             >
-              Update
-            </Button>
-          </>
-        )}
-      </Formik>
-    </AppScreen>
+              {({
+                handleChange,
+                handleSubmit,
+                values,
+                setFieldTouched,
+                touched,
+                errors,
+              }) => (
+                <>
+                  {/** tax points and commission rate */}
+                  <View style={styles.taxPointsAndCommissionRateContainer}>
+                    {/** tax */}
+                    <TextInput
+                      style={styles.taxPointsInput}
+                      label="Tax Points"
+                      onChangeText={handleChange("taxPoints")}
+                      onBlur={() => setFieldTouched("taxPoints")}
+                      value={values.taxPoints}
+                      keyboardType="numeric"
+                      autoCorrect={false}
+                    />
+                    {errors["taxPoints"] && touched["taxPoints"] ? (
+                      <Text style={styles.errorMessage}>
+                        {errors["taxPoints"]}
+                      </Text>
+                    ) : null}
+                    {/** commission */}
+                    <TextInput
+                      style={styles.commissionRateInput}
+                      label="Commission Rate"
+                      onChangeText={handleChange("commissionRate")}
+                      onBlur={() => setFieldTouched("commissionRate")}
+                      value={values.commissionRate}
+                      keyboardType="numeric"
+                      autoCorrect={false}
+                    />
+                    {errors["commissionRate"] && touched["commissionRate"] ? (
+                      <Text style={styles.errorMessage}>
+                        {errors["commissionRate"]}
+                      </Text>
+                    ) : null}
+                  </View>
+                  {/** insurances */}
+                  <Text style={styles.insurancesTitle}>Insurances</Text>
+                  {/** compulsory */}
+                  <TextInput
+                    label="Compulsory Insurance"
+                    onChangeText={handleChange("compulsoryInsurance")}
+                    onBlur={() => setFieldTouched("compulsoryInsurance")}
+                    value={values.compulsoryInsurance}
+                    keyboardType="numeric"
+                    autoCorrect={false}
+                  />
+                  {errors["compulsoryInsurance"] &&
+                  touched["compulsoryInsurance"] ? (
+                    <Text style={styles.errorMessage}>
+                      {errors["compulsoryInsurance"]}
+                    </Text>
+                  ) : null}
+                  {/** collateral */}
+                  <TextInput
+                    style={styles.textInput}
+                    label="Collateral Insurance"
+                    onChangeText={handleChange("collateralInsurance")}
+                    onBlur={() => setFieldTouched("collateralInsurance")}
+                    value={values.collateralInsurance}
+                    keyboardType="numeric"
+                    autoCorrect={false}
+                  />
+                  {errors["collateralInsurance"] &&
+                  touched["collateralInsurance"] ? (
+                    <Text style={styles.errorMessage}>
+                      {errors["collateralInsurance"]}
+                    </Text>
+                  ) : null}
+                  {/** personal */}
+                  <TextInput
+                    style={styles.textInput}
+                    label="Personal Insurance"
+                    onChangeText={handleChange("personalInsurance")}
+                    onBlur={() => setFieldTouched("personalInsurance")}
+                    value={values.personalInsurance}
+                    keyboardType="numeric"
+                    autoCorrect={false}
+                  />
+                  {errors["personalInsurance"] &&
+                  touched["personalInsurance"] ? (
+                    <Text style={styles.errorMessage}>
+                      {errors["personalInsurance"]}
+                    </Text>
+                  ) : null}
+                  {/** update btn */}
+                  <Button
+                    style={styles.updateBtn}
+                    mode="contained"
+                    onPress={handleSubmit}
+                    loading={isLoading}
+                  >
+                    Update
+                  </Button>
+                </>
+              )}
+            </Formik>
+          </View>
+        </TouchableWithoutFeedback>
+      </AppScreen>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -168,7 +191,6 @@ const styles = StyleSheet.create({
     padding: AppSpacing.m,
   },
   text: {
-    marginTop: AppSpacing.s,
     alignSelf: "center",
   },
   profileDetailsContainer: {
