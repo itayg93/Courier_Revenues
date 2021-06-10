7e2d5f1fc776190db57dfb4382333100badce735
Itay <itayg93@gmail.com>
2021-06-10 07:55:42 +0300

    Add chevron-right to each expense.type, cost, comment?

--- app/components/ExpenseCard.js
+++ app/components/ExpenseCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import { AppColors, AppSpacing, AppSizes } from "../config";
 
@@ -15,11 +16,21 @@ export const ExpenseCard = ({ expense }) => {
       </View>
       {/** type */}
       <View style={styles.rowContainer}>
+        <MaterialCommunityIcons
+          name="chevron-right"
+          size={20}
+          color={AppColors.medium}
+        />
         <Text style={styles.title}>Type: </Text>
         <Text style={styles.data}>{expense.type}</Text>
       </View>
       {/** cost */}
       <View style={styles.rowContainer}>
+        <MaterialCommunityIcons
+          name="chevron-right"
+          size={20}
+          color={AppColors.medium}
+        />
         <Text style={styles.title}>Cost: </Text>
         <Text style={styles.data}>{expense.cost + " ₪"}</Text>
       </View>
@@ -27,6 +38,11 @@ export const ExpenseCard = ({ expense }) => {
       <View style={styles.rowContainer}>
         {expense.comment ? (
           <>
+            <MaterialCommunityIcons
+              name="chevron-right"
+              size={20}
+              color={AppColors.medium}
+            />
             <Text style={styles.title}>Comment: </Text>
             <Text style={styles.data}>{expense.comment}</Text>
           </>
